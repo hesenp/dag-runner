e41a2de7fd8cec2d12da50ef718aff6769290aee
Hesen Peng <hesen.peng@gmail.com>
2014-03-24 22:41:05 -0700

    added files created by leiningen

---
+++ src/dag_runner/core.clj
@@ -0,0 +1,6 @@
+(ns dag-runner.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/dag_runner/core_test.clj
@@ -0,0 +1,7 @@
+(ns dag-runner.core-test
+  (:require [clojure.test :refer :all]
+            [dag-runner.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
